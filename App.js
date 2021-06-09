1fc5ae017d598ab51d849005b8769d631dd256c8
Anna Kocot <annakocot@tutamail.com>
2021-06-09 20:02:05 +0200

    menu implementation

--- App.js
+++ App.js
@@ -1,5 +1,14 @@
 const startBtn = document.querySelector('.new-game-button');
+const gameBoard = document.querySelector('.game-board');
+const menu = document.querySelector('.menu-container');
 
-startBtn.addEventListener('click', () => console.log('STTART'));
+const startGame = () => {
+  const boardSizeValue = document.getElementById('board-size').options[document.getElementById('board-size').selectedIndex].value;
 
+  gameBoard.classList.add('visible');
+  menu.classList.add('hidden');
+  gameBoard.style.height = `${boardSizeValue}px`;
+  gameBoard.style.width = `${boardSizeValue}px`;
+};
 
+startBtn.addEventListener('click', startGame);
