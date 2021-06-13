e900f022af237b15c5d521c1926df96edc61822e
Bartosz Mazewski <mazewski.bartosz@gmail.com>
2021-06-13 11:19:26 +0200

    add movement logic

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 const startBtn = document.querySelector('.new-game-button');
-const gameBoard = document.querySelector('.game-board');
+const canvas = document.querySelector('#game-board');
 const menu = document.querySelector('.menu-container');
 
 
@@ -7,45 +7,85 @@ const startGame = () => {
   const boardSizeValue = document.getElementById('board-size').options[document.getElementById('board-size').selectedIndex].value;
   const snakeStartSpeed = document.getElementById('board-size').options[document.getElementById('start-speed').selectedIndex].value;
 
+
   (function gameSettings() {
-    gameBoard.classList.add('visible');
+    canvas.classList.add('visible');
     menu.classList.add('hidden');
-    gameBoard.style.height = `${boardSizeValue}px`;
-    gameBoard.style.width = `${boardSizeValue}px`;
-   })(boardSizeValue, snakeStartSpeed);
-
-   class Snake {
-     constructor(){
-      this.snakePosition = [
-      { x: 100, y: 100},
-      { x: 90, y: 100},
-      { x: 80, y: 100},
-      { x: 70, y: 100}];
-
-    //  handleSnakeMove(){
-
-     };
-
-     handleSnakeDraw(){
-      const canvas = document.getElementsByClassName('game-board');
-      const ctx = canvas[0].getContext('2d');
-      ctx.strokeRect(50 , 50, unit, unit);
-     };
+    canvas.setAttribute('height', `${boardSizeValue}px`);
+    canvas.setAttribute('width', `${boardSizeValue}px`);
+  })(boardSizeValue, snakeStartSpeed);
 
 
-  // 1. metoda - rysowanie
-  // 2. metoda - zmiana pozycji
+  ctx = canvas.getContext('2d');
+  ctx.fillStyle = 'hotpink';
+  const unit = 20;
 
+  let snake = [{
+      x: 100,
+      y: 100
+    },
+    {
+      x: 80,
+      y: 100
+    },
+    {
+      x: 60,
+      y: 100
+    },
+    {
+      x: 40,
+      y: 100
+    }
+  ];
 
-   }
+  let changingDirection = false;
+  let dx = unit;
+  let dy = 0;
 
-  const unit = 10;
-  const deltaX = unit;
-  //const deltaY;
+  document.addEventListener("keydown", changeDirection);
 
+  function changeDirection(event) {
+    const W_KEY = 87;
+    const S_KEY = 83;
+    const A_KEY = 65;
+    const D_KEY = 68;
 
+    if (changingDirection) return;
+    changingDirection = true;
+    const keyPressed = event.keyCode;
+    const goingUp = dy === -unit;
+    const goingDown = dy === unit;
+    const goingRight = dx === unit;
+    const goingLeft = dx === -unit;
 
+    if (keyPressed === A_KEY && !goingRight) {
+      dx = -unit;
+      dy = 0;
+    }
+    if (keyPressed === W_KEY && !goingDown) {
+      dx = 0;
+      dy = -unit;
+    }
+    if (keyPressed === D_KEY && !goingLeft) {
+      dx = unit;
+      dy = 0;
+    }
+    if (keyPressed === S_KEY && !goingUp) {
+      dx = 0;
+      dy = unit;
+    }
+  }
 
+  function moveSnake() {
+    const head = {
+      x: snake[0].x + dx,
+      y: snake[0].y + dy
+    };
+    snake.unshift(head);
+    snake.pop();
+  }
+
+  moveSnake();
 
 };
 
