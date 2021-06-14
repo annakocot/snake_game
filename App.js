777ccdeb0068655f187f4e48aa447801614f6a7e
Liliia Panchyshyn <panchyshyn.liliia@gmail.com>
2021-06-14 14:17:34 +0200

    added apple to the screen

--- App.js
+++ App.js
@@ -13,25 +13,41 @@ const startGame = () => {
     menu.classList.add('hidden');
     canvas.setAttribute('height', `${boardSizeValue}px`);
     canvas.setAttribute('width', `${boardSizeValue}px`);
-   })(boardSizeValue, snakeStartSpeed);
+  })(boardSizeValue, snakeStartSpeed);
 
 
   ctx = canvas.getContext('2d');
   ctx.fillStyle = 'hotpink';
   const unit = 20;
 
-  let snake = [ {x: 100, y: 100},  {x: 80, y: 100},  {x: 60, y: 100},  {x: 40, y: 100} ];
+  let widthblock = boardSizeValue / unit;
+  let heightblock = boardSizeValue / unit;
+  let food = {
+    x: 0,
+    y: 0
+  }
+  function drawFood() {
+    ctx.fillStyle = "red";
+    ctx.fillRect(food.x, food.y, unit, unit);
+    ctx.strokeRect(food.x, food.y, unit, unit);
+  }
+  //  Bartosz Mazewski13:54
+  //  (Math.round((Math.random() * (boardSizeValue) + min) / unit) * 10) * 10;
+  //  Patrycja P13:56
+  //  (Math.round((Math.random() * (boardSizeValue) / unit) * 10) * 10;
 
-  function drawSnakePart(snakePart){
+  let snake = [{ x: 100, y: 100 }, { x: 80, y: 100 }, { x: 60, y: 100 }, { x: 40, y: 100 }];
+
+  function drawSnakePart(snakePart) {
     ctx.fillRect(snakePart.x, snakePart.y, unit, unit);
     ctx.strokeRect(snakePart.x, snakePart.y, unit, unit);
   }
+  function drawSnake() {
+    snake.forEach(drawSnakePart);
+  }
 
-  function drawSnake(){
-      snake.forEach(drawSnakePart);
-}
-
-drawSnake();
+  drawSnake();
+  drawFood();
 };
 
 startBtn.addEventListener('click', startGame);
