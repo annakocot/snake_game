2d072e8c1c2c81d416f60315d3166f0aa4fb3e7a
Anna Kocot <annakocot@tutamail.com>
2021-06-14 13:26:03 +0200

    add snake head

--- App.js
+++ App.js
@@ -2,36 +2,52 @@ const startBtn = document.querySelector('.new-game-button');
 const canvas = document.querySelector('#game-board');
 const menu = document.querySelector('.menu-container');
 
-
 const startGame = () => {
-  const boardSizeValue = document.getElementById('board-size').options[document.getElementById('board-size').selectedIndex].value;
-  const snakeStartSpeed = document.getElementById('board-size').options[document.getElementById('start-speed').selectedIndex].value;
-
+  const boardSizeValue =
+    document.getElementById('board-size').options[
+      document.getElementById('board-size').selectedIndex
+    ].value;
+  const snakeStartSpeed =
+    document.getElementById('board-size').options[
+      document.getElementById('start-speed').selectedIndex
+    ].value;
 
   (function gameSettings() {
     canvas.classList.add('visible');
     menu.classList.add('hidden');
     canvas.setAttribute('height', `${boardSizeValue}px`);
     canvas.setAttribute('width', `${boardSizeValue}px`);
-   })(boardSizeValue, snakeStartSpeed);
-
+  })(boardSizeValue, snakeStartSpeed);
 
   ctx = canvas.getContext('2d');
-  ctx.fillStyle = 'hotpink';
   const unit = 20;
 
-  let snake = [ {x: 100, y: 100},  {x: 80, y: 100},  {x: 60, y: 100},  {x: 40, y: 100} ];
+  let snakeHead = [{ x: 100, y: 100 }];
+  let snakeBody = [
+    { x: 100, y: 100 },
+    { x: 80, y: 100 },
+    { x: 60, y: 100 },
+    { x: 40, y: 100 },
+  ];
 
-  function drawSnakePart(snakePart){
+  function drawSnakeBody(snakePart) {
+    ctx.fillStyle = '#11d147';
     ctx.fillRect(snakePart.x, snakePart.y, unit, unit);
     ctx.strokeRect(snakePart.x, snakePart.y, unit, unit);
   }
 
-  function drawSnake(){
-      snake.forEach(drawSnakePart);
-}
+  function drawSnakeHead(snakePart) {
+    ctx.fillStyle = '#0b842d';
+    ctx.fillRect(snakePart.x, snakePart.y, unit, unit);
+    ctx.strokeRect(snakePart.x, snakePart.y, unit, unit);
+  }
 
-drawSnake();
+  function drawSnake() {
+    snakeBody.forEach(drawSnakeBody);
+    snakeHead.forEach(drawSnakeHead);
+  }
+
+  drawSnake();
 };
 
 startBtn.addEventListener('click', startGame);
